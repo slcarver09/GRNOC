d553c445302933eab8bacc5948d13734a998424a
slcarver09 <94541329+slcarver09@users.noreply.github.com>
2025-07-21 08:58:58 -0400

    Added CID Grabber
    Quick script to grab CIDs from DB

---
+++ cid-grabbyer.py
@@ -0,0 +1,40 @@
+from globalnoc import wsc
+import getpass
+import csv
+
+
+#create WSC connection
+client = wsc.WSC()
+client.username = input("Enter username: ")
+client.password = getpass.getpass('Enter password: ')
+client.url = "https://db.net.internet2.edu/cds2/circuit.cgi"
+client.realm = "https://idp.net.internet2.edu/idp/profile/SAML2/SOAP/ECP"
+
+
+circuitNames = []
+
+
+#open csv
+with open('circuits.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        name = row.get('circuit_name')
+        if name:
+            circuitNames.append(name)
+
+#iterate through list print CID:
+#currently need to update NETWORK_ID EVERYTIME
+for circuitName in circuitNames:
+
+    data = client.get_circuits(network_id = 50, name = circuitName)
+
+    results = data.get("results", [])
+
+    for circuit in results:
+        if results:
+            cid = results[0]
+            circuit_id =  cid.get("circuit_id")
+            print(f"Circuit Name: {circuitName} - Circuit ID: {circuit_id}")
+        else:
+            print("No circuits found.")
+
